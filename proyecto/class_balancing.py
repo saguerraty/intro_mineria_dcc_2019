da49396798ac13fd0646a885259fe43baa89cae6
basty <bastian.inostroza@gmail.com>
2019-10-09 19:05:40 -0300

    Falta hacerse cargo de los NaN para que corra el Fit

--- proyecto/class_balancing.py
+++ proyecto/class_balancing.py
@@ -1,11 +1,12 @@
 
 
 #%%
-from imblearn.over_sampling import SMOTENC 
+from collections import Counter
+from imblearn.over_sampling import SMOTENC
 import pandas as pd
 
 #%%
-tabla_imbalanced = pd.read_csv("C:/Users/sague/Documents/GitHub/intro_mineria_dcc_2019/proyecto/tabla_imbalanced.csv")
+tabla_imbalanced = pd.read_csv("./tabla_imbalanced.csv")
 
 #%%
 tabla_imbalanced.dtypes
@@ -15,4 +16,11 @@ columns = list(tabla_imbalanced)
 categories = tabla_imbalanced['cat_mp10'].drop_duplicates().tolist()
 
 #%%
-balanced_df = SMOTENC(['cat_mp10','cat_mp25'])
+smotenc = SMOTENC(['cat_mp10','cat_mp25'])
+
+X = tabla_imbalanced.iloc[:, 0:13]
+y = tabla_imbalanced.loc[:, 'cat_mp10']
+
+X_res, y_res = smotenc.fit_resample(X, y)
+print(f'Resampled dataset samples per class{Counter(y_res)}')
+
