138ea03b715af79a9817d71f4723bdd5ce12491f
BastyZ <bastian.inostroza@gmail.com>
2019-10-11 23:33:57 -0300

    AÑADIDO CSV CON CATEGORIAS NUMERICAS

--- proyecto/class_balancing.py
+++ proyecto/class_balancing.py
@@ -3,7 +3,7 @@ from imblearn.over_sampling import SMOTENC
 import pandas as pd
 
 
-tabla_imbalanced = pd.read_csv("./tabla_imbalanced.csv")
+tabla_imbalanced = pd.read_csv("./tabla_imbalanced.csv").drop(columns=['Fecha'])
 
 # we drop SO2, MP25, and NO2 due to much NaN
 #tabla_imbalanced = tabla_imbalanced.drop(columns=['SO2', 'MP25', 'NO2'])
@@ -19,10 +19,10 @@ categories = tabla_imbalanced['cat_mp10'].drop_duplicates().tolist()
 nan_count = tabla_imbalanced.isna().sum()
 
 # %%
-smotenc = SMOTENC(['cat_mp10', 'cat_mp25'])
+smotenc = SMOTENC(categorical_features=[10])
 
-X = tabla_imbalanced.iloc[:, 0:12]
-y = tabla_imbalanced.loc[:, 'cat_mp10']
+X = tabla_imbalanced.iloc[:, 0:11]
+y = tabla_imbalanced.iloc[:, 9:10]
 
 X_res, y_res = smotenc.fit_resample(X, y)
 print(f'Resampled dataset samples per class{Counter(y_res)}')
